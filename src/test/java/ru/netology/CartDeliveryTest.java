0a5813fe160a40f481781f076d9f258fe265a3ee
Aleksandra Kalerina <aleksandra.bm@mail.ru>
2024-06-28 22:07:47 +0300

    First commit error

---
+++ src/test/java/ru/netology/CartDeliveryTest.java
@@ -0,0 +1,33 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class CartDeliveryTest {
+
+    private String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+@Test
+    public void inputForm() {
+    open("http://0.0.0.0:9999");
+    $("[data-test-id='city'] input").setValue("Самара");
+    String datePlan = generateDate(4, "dd.MM.yyyy");
+    $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+    $("[data-test-id='date'] input").setValue(datePlan);
+    $("[data-test-id='name'] input").setValue("Жук-Алексей Алексеевич");
+    $("[data-test-id='phone'] input").setValue("+79270000000");
+    $("[data-test-id='agreement'] input").click();
+    $("button.button").click();
+    $(".notification__content")
+            .shouldBe(Condition.visible, Duration.ofSeconds(15))
+            .shouldHave(Condition.text("Встреча успешно забронирована на " + datePlan));
+}
+}
